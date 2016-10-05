baa12ff7acc2fe3f3f044a25490f83e571f87441
Dynami27 <Neuron@khalids-macbook-pro.local>
2016-10-05 11:40:12 -0500

    Initial Commit

---
+++ UITextview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITextview
+//
+//  Created by Khalid Mohamed on 10/5/16.
+//  Copyright © 2016 Khalid Mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITextview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextview
+//
+//  Created by Khalid Mohamed on 10/5/16.
+//  Copyright © 2016 Khalid Mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
